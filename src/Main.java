d3b003685b3f4a575905421ef4d7acb047d7a00f
flerxay <bettercallfler@gmail.com>
2024-05-03 13:42:02 -0400

    valores guardados en variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
+        System.out.println("¡Bienvenidos a la inmersión en Java!");
+        System.out.println("Película Matrix");
+
+        int fechaDeLanzamiento;
+        fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+
     }
 }
