0d91107b8011489fde694a2c3e63cd719f826c7d
flerxay <bettercallfler@gmail.com>
2024-05-03 19:50:25 -0400

    desafio completado por flerr

--- src/modelo/Principal.java
+++ src/modelo/Principal.java
@@ -4,44 +4,71 @@ import java.util.Scanner;
 
 public class Principal {
 
-    public void muestraElMenu(){
+    private Scanner teclado = new Scanner(System.in);
+
+    public void muestraElMenu() {
         int opcion = 0;
-        Scanner teclado = new Scanner(System.in);
-        while(opcion != 9) {
-            String menu = """
+        while (opcion != 9) {
+            System.out.println("""
                     Bienvenid@ a ScreenMatch
                     1) Registrar nueva película
                     2) Registrar una nueva serie
-                    
+                    3) Calculadora de tiempo de maratón
                     9) Salir
-                    """;
-            System.out.println(menu);
+                    """);
             opcion = teclado.nextInt();
             teclado.nextLine();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
-                    System.out.println("Ingrese nombre de película");
-                    String nombre = teclado.nextLine();
-                    System.out.println("Ingrese el año de lanzamiento de la película");
-                    int fechaDeLanzamiento = teclado.nextInt();
-                    teclado.nextLine();
-                    System.out.println("Ingresa la duración en minutos de la película");
-                    int duracionEnMinutos = teclado.nextInt();
-                    teclado.nextLine();
-                    Pelicula peliculaUsuario = new Pelicula();
-                    peliculaUsuario.setNombre(nombre);
-                    peliculaUsuario.setFechaDeLanzamiento(fechaDeLanzamiento);
-                    peliculaUsuario.setTiempoDeDuracionEnMinutos(duracionEnMinutos);
-                    peliculaUsuario.muestraFichaTecnica();
+                    registrarTitulo(new Pelicula());
                     break;
-                default:
-                    System.out.println("Opción no válida");
+                case 2:
+                    registrarTitulo(new Serie());
+                    break;
+                case 3:
+                    calculadoraDeMaraton();
                     break;
                 case 9:
                     System.out.println("Saliendo del Programa...");
                     break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
             }
         }
     }
-}
 
+    private void registrarTitulo(Titulo titulo) {
+        System.out.println("Ingrese nombre del título:");
+        titulo.setNombre(teclado.nextLine());
+        System.out.println("Ingrese el año de lanzamiento:");
+        titulo.setFechaDeLanzamiento(teclado.nextInt());
+        teclado.nextLine();
+        if (titulo instanceof Pelicula) {
+            System.out.println("Ingresa la duración en minutos de la película:");
+            ((Pelicula) titulo).setTiempoDeDuracionEnMinutos(teclado.nextInt());
+            teclado.nextLine();
+        } else if (titulo instanceof Serie) {
+            System.out.println("Ingresa el número de temporadas:");
+            ((Serie) titulo).setTemporadas(teclado.nextInt());
+            System.out.println("Ingresa el número de episodios por temporada:");
+            ((Serie) titulo).setEpisodiosPorTemporada(teclado.nextInt());
+            System.out.println("Ingresa la duración en minutos por episodio:");
+            ((Serie) titulo).setDuracionEnMinutosPorEpisodio(teclado.nextInt());
+            teclado.nextLine();
+            System.out.println("Ingresa la calificación de la serie (0 a 5), por ejemplo, 4.2:");
+            ((Serie) titulo).setCalificacion(teclado.nextDouble());
+            teclado.nextLine();
+        }
+        titulo.muestraFichaTecnica();
+    }
+
+    private void calculadoraDeMaraton() {
+        System.out.println("Calculadora de tiempo de maratón");
+        System.out.println("Ingresa el número total de minutos de series y películas:");
+        int totalMinutos = teclado.nextInt();
+        int dias = totalMinutos / (24 * 60);
+        int horas = (totalMinutos % (24 * 60)) / 60;
+        System.out.printf("Necesitarás %d días y %d horas para completar el maratón.%n", dias, horas);
+    }
+}
--- src/modelo/Serie.java
+++ src/modelo/Serie.java
@@ -3,10 +3,9 @@ package modelo;
 public class Serie extends Titulo {
 
     private int temporadas;
-
     private int episodiosPorTemporada;
-
     private int duracionEnMinutosPorEpisodio;
+    private double calificacion; // Atributo para la calificación de la serie
 
     @Override
     public int getTiempoDeDuracionEnMinutos() {
@@ -36,5 +35,16 @@ public class Serie extends Titulo {
     public void setDuracionEnMinutosPorEpisodio(int duracionEnMinutosPorEpisodio) {
         this.duracionEnMinutosPorEpisodio = duracionEnMinutosPorEpisodio;
     }
-}
 
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        if (calificacion >= 0 && calificacion <= 5) {
+            this.calificacion = calificacion;
+        } else {
+            System.out.println("La calificación debe estar entre 0 y 5.");
+        }
+    }
+}
