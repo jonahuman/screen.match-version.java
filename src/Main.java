b0bfadb0f468d9704afc536f892211d58c5fa6a7
flerxay <bettercallfler@gmail.com>
2024-05-03 17:39:23 -0400

    modificando aula-02

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Bienvenidos a la inmersión en Java!");
-         System.out.println("Película Matrix");
-        // System.out.println("Película Matrix");
-        // Declaración de variables
-        int fechaDeLanzamiento;
-        fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico;
-        incluidoEnElPlanBasico = true;
-        String nombre = "Matrix";
-        String sinopsis = """
-                El programador informático Thomas Anderson, 
-                más conocido en el mundo de los "hacker" como Neo, 
-                está en el punto de mira del temible agente Smith. 
-                Otros dos piratas informáticos, Trinity y Morfeo, se ponen en contacto con Neo para ayudarlo a escapar. 
-                Matrix te posee. Sigue al conejo blanco.
-                """;
-        double mediaEvaluacionUsuario = 0;
-        System.out.println("Película: " + nombre);
-        System.out.println("Fecha: " + fechaDeLanzamiento);
-        System.out.println("Puntaje: " + evaluacion);
-        System.out.println("Plan: " + incluidoEnElPlanBasico);
+        Pelicula matrix = new Pelicula();
+        matrix.nombre = "Matrix";
+        matrix.tiempoDeDuracionEnMinutos = 125;
+        matrix.fechaDeLanzamiento = 1999;
 
-        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
-        System.out.println("Media de la Evaluación de Matrix: " + mediaEvaluacion);
+        System.out.println(matrix.nombre);
+        System.out.println(matrix.tiempoDeDuracionEnMinutos);
+        System.out.println(matrix.fechaDeLanzamiento);
 
-        if (fechaDeLanzamiento >= 2023) {
-            System.out.println("Película popular en el momento");
-        } else {
-            System.out.println("Película retro que vale la pena ver");
-        }
+        Pelicula encanto =  new Pelicula();
+        encanto.nombre = "Encanto";
+        encanto.tiempoDeDuracionEnMinutos = 120;
+        encanto.fechaDeLanzamiento = 2022;
 
-        for (int i = 0; i < 3; i++) { // Ciclo for visto en la primera clase, se cambiará a While en 'MainConWhile.java'
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingresa la nota que le darías a Matrix: ");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix; // En consola, para los décimales intente usar coma (,)
-                                                                          // En lugar de (.) si le da error y viceversa.
-        }
-        System.out.println("La media de la película " +
-                "Matrix calculada por el usuario es: " + mediaEvaluacionUsuario / 3);
+        System.out.println();
     }
 }
 
--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,17 @@
 public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    double evaluacion;
+    int tiwmpoDeDuracionEnMinutos;
+    boolean incluidoEnPLanBasico = true;
+    String sinopsis;
+
+    void muestraFichaTecnica() {
+        // Creando mi ficha técnica con concatenación.
+        System.out.println("-.-.-.-.-.-FICHA TÉCNICA-.-.-.-.-.-.-");
+        System.out.println("Nombre de la película: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Tiempo de duración: " + tiwmpoDeDuracionEnMinutos + " minutos");
+
+     }
 }
