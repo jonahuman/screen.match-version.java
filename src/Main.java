0609e172053e9d1bb4c504594dcc1553a18a5e17
flerxay <bettercallfler@gmail.com>
2024-05-03 18:38:28 -0400

    agregamos serie junto a pelicula

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 import modelo.Pelicula;
+import modelo.Serie;
 
-public class Main {
-    public static void main(String[] args) {
-        Pelicula matrix = new Pelicula();
-        matrix.setNombre("Matrix");
-        matrix.setTiempoDeDuracionEnMinutos(120);
-        matrix.setFechaDeLanzamiento(1999);
+    public class Main {
+        public static void main(String[] args) {
+            Pelicula matrix = new Pelicula();
+            matrix.setNombre("Matrix");
+            matrix.setTiempoDeDuracionEnMinutos(120);
+            matrix.setFechaDeLanzamiento(1999);
 
-        matrix.muestraFichaTecnica();
+            matrix.muestraFichaTecnica();
 
+            Serie friends = new Serie();
+            friends.setNombre("Friends");
+            friends.setFechaDeLanzamiento(1994);
+            friends.setTemporadas(10);
+            friends.setEpisodiosPorTemporada(22);
+            friends.setDuracionEnMinutosPorEpisodio(30);
+
+            friends.muestraFichaTecnica();
+
+
+        }
     }
-}
+
+
+
 
 // @author: Jonathan David Ahumada Novoa
 // Fecha: 3 de Mayo, 2024
---
+++ src/modelo/Serie.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Serie extends Titulo {
+
+    private int temporadas;
+
+    private int episodiosPorTemporada;
+
+    private int duracionEnMinutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getDuracionEnMinutosPorEpisodio() {
+        return duracionEnMinutosPorEpisodio;
+    }
+
+    public void setDuracionEnMinutosPorEpisodio(int duracionEnMinutosPorEpisodio) {
+        this.duracionEnMinutosPorEpisodio = duracionEnMinutosPorEpisodio;
+    }
+}
+
